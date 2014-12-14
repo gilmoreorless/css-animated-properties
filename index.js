4a80f5f79f75a33b8a60678a9a77d197c7497a06
Gilmore Davidson <gilmoreorless@gmail.com>
2014-12-15 09:18:44 +1100

    Switched data source to proper W3C specs and did some cleanup
    New properties added:
    * border-spacing
    * clip-path
    * mask
    * mask-position
    * mask-size
    * text-emphasis
    * text-emphasis-color
    * tab-size

--- index.js
+++ index.js
@@ -4,52 +4,64 @@
  */
 (function (exports) {
     /**
-     * Data taken from https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_animated_properties
-     * If a property is not in this list, it's not able to be animated.
+     * Data collated from multiple W3C specs: http://www.w3.org/Style/CSS/current-work
+     * Only specs that are Candidate Recommendations or better are counted, with the
+     * exception of some Working Drafts that have a lot of traction in browser implementations.
+     * So far the WD specs included here are Text Level 3, Flexbox and Transforms.
+     *
+     * If a property is not in this list, it's not able to be consistently animated.
+     * Technically `background-image` can be animated in some ways, but against the spec.
+     *
      * @type {Object}
      */
     var props = exports.animatedProperties = {
-        // Transforms
-        'transform': {types: ['transform']},
-        'transform-origin': {types: ['length', 'percentage', 'calc'], multiple: true},
-        'perspective': {types: ['length']},
-        'perspective-origin': {types: ['length', 'percentage', 'calc'], multiple: true},
 
-        // Color
+        /*** Recommendations (REC) ***/
+
+        // CSS 2.1: http://www.w3.org/TR/css3-transitions/#animatable-types
+        // --Box model
+        'margin': {properties: ['margin-top', 'margin-right', 'margin-bottom', 'margin-left']},
+        'margin-bottom': {types: ['length']},
+        'margin-left': {types: ['length']},
+        'margin-right': {types: ['length']},
+        'margin-top': {types: ['length']},
+        'padding': {properties: ['padding-top', 'padding-right', 'padding-bottom', 'padding-left']},
+        'padding-bottom': {types: ['length']},
+        'padding-left': {types: ['length']},
+        'padding-right': {types: ['length']},
+        'padding-top': {types: ['length']},
+        // --Visual formatting model
+        'bottom': {types: ['length-percentage-calc']},
+        'left': {types: ['length-percentage-calc']},
+        'right': {types: ['length-percentage-calc']},
+        'top': {types: ['length-percentage-calc']},
+        'z-index': {types: ['integer']},
+        // --Visual formatting model details
+        'width': {types: ['length-percentage-calc']},
+        'max-width': {types: ['length-percentage-calc']},
+        'min-width': {types: ['length-percentage-calc']},
+        'height': {types: ['length-percentage-calc']},
+        'max-height': {types: ['length-percentage-calc']},
+        'min-height': {types: ['length-percentage-calc']},
+        'line-height': {types: ['number', 'length']},
+        'vertical-align': {types: ['length']},
+        // --Visual effects
+        'visibility': {types: ['visibility']},
+        // --Tables
+        'border-spacing': {types: ['length'], multiple: true},
+
+        // CSS Color Module Level 3: http://www.w3.org/TR/css3-color/
         'color': {types: ['color']},
         'opacity': {types: ['number']},
 
-        // Columns
-        'columns': {properties: ['column-width', 'column-count']},
-        'column-width': {types: ['length']},
-        'column-count': {types: ['integer']},
-        'column-gap': {types: ['length']},
-        'column-rule': {properties: ['column-rule-color', 'column-rule-width']},
-        'column-rule-color': {types: ['color']},
-        'column-rule-width': {types: ['length']},
 
-        // Text
-        'letter-spacing': {types: ['length']},
-        'text-indent': {types: ['length', 'percentage', 'calc']},
-        'word-spacing': {types: ['length']},
+        /*** Candidate Recommendations (CR) ***/
 
-        // Text decorations
-        'text-decoration': {properties: ['text-decoration-color']},
-        'text-decoration-color': {types: ['color']},
-        'text-shadow': {types: ['shadow-list']},
-
-        // Flexible boxes
-        'flex': {properties: ['flex-grow', 'flex-shrink', 'flex-basis']},
-        'flex-grow': {types: ['number']},
-        'flex-shrink': {types: ['number']},
-        'flex-basis': {types: ['length', 'percentage', 'calc']},
-        'order': {types: ['integer']},
-
-        // Background & Borders
+        // CSS Backgrounds and Borders Module Level 3: http://www.w3.org/TR/css3-background/
         'background': {properties: ['background-color', 'background-position', 'background-size']},
         'background-color': {types: ['color']},
-        'background-position': {types: ['length', 'percentage', 'calc'], multiple: true, repeatable: true},
-        'background-size': {types: ['length', 'percentage', 'calc'], multiple: true, repeatable: true},
+        'background-position': {types: ['length-percentage-calc'], multiple: true, repeatable: true},
+        'background-size': {types: ['length-percentage-calc'], multiple: true, repeatable: true},
         // --Border combinations
         'border': {properties: ['border-color', 'border-width']},
         'border-bottom': {properties: ['border-bottom-color', 'border-bottom-width']},
@@ -69,62 +81,78 @@
         'border-top-width': {types: ['length']},
         // --Border radius
         'border-radius': {properties: ['border-top-left-radius', 'border-top-right-radius', 'border-bottom-right-radius', 'border-bottom-left-radius']},
-        'border-top-left-radius': {types: ['length', 'percentage', 'calc']},
-        'border-top-right-radius': {types: ['length', 'percentage', 'calc']},
-        'border-bottom-right-radius': {types: ['length', 'percentage', 'calc']},
-        'border-bottom-left-radius': {types: ['length', 'percentage', 'calc']},
-
-        // Box model
+        'border-top-left-radius': {types: ['length-percentage-calc'], multiple: true},
+        'border-top-right-radius': {types: ['length-percentage-calc'], multiple: true},
+        'border-bottom-right-radius': {types: ['length-percentage-calc'], multiple: true},
+        'border-bottom-left-radius': {types: ['length-percentage-calc'], multiple: true},
+        // --Box shadow
         'box-shadow': {types: ['shadow-list']},
-        'margin': {properties: ['margin-top', 'margin-right', 'margin-bottom', 'margin-left']},
-        'margin-bottom': {types: ['length']},
-        'margin-left': {types: ['length']},
-        'margin-right': {types: ['length']},
-        'margin-top': {types: ['length']},
-        'padding': {properties: ['padding-top', 'padding-right', 'padding-bottom', 'padding-left']},
-        'padding-bottom': {types: ['length']},
-        'padding-left': {types: ['length']},
-        'padding-right': {types: ['length']},
-        'padding-top': {types: ['length']},
-        'height': {types: ['length', 'percentage', 'calc']},
-        'max-height': {types: ['length', 'percentage', 'calc']},
-        'min-height': {types: ['length', 'percentage', 'calc']},
-        'width': {types: ['length', 'percentage', 'calc']},
-        'max-width': {types: ['length', 'percentage', 'calc']},
-        'min-width': {types: ['length', 'percentage', 'calc']},
-        'visibility': {types: ['visibility']},
 
-        // Table
-        'vertical-align': {types: ['length']},
-
-        // Positioning
-        'bottom': {types: ['length', 'percentage', 'calc']},
-        'left': {types: ['length', 'percentage', 'calc']},
-        'right': {types: ['length', 'percentage', 'calc']},
-        'top': {types: ['length', 'percentage', 'calc']},
-        'z-index': {types: ['integer']},
-
-        // Fonts
-        'font': {properties: ['font-weight', 'font-stretch', 'font-size', 'line-height']},
-        'font-weight': {types: ['font-weight']},
-        'font-stretch': {types: ['font-stretch']},
-        'font-size': {types: ['length']},
-        'line-height': {types: ['number', 'length']},
-        'font-size-adjust': {types: ['number']},
-
-        // User interface
+        // CSS Basic User Interface Module Level 3 (CSS3 UI): http://www.w3.org/TR/css3-ui/
         'outline': {properties: ['outline-color', 'outline-width']},
         'outline-color': {types: ['color']},
         'outline-width': {types: ['length']},
         'outline-offset': {types: ['length']},
 
-        // Miscellaneous
-        'clip': {types: ['rectangle']},
+        // CSS Fonts Module Level 3: http://www.w3.org/TR/css3-fonts/
+        'font': {properties: ['font-weight', 'font-stretch', 'font-size', 'line-height']},
+        'font-weight': {types: ['font-weight']},
+        'font-stretch': {types: ['font-stretch']},
+        'font-size': {types: ['length']},
+        'font-size-adjust': {types: ['number']},
 
-        // Shapes
+        // CSS Masking Module Level 1: http://www.w3.org/TR/css-masking/
+        'clip': {types: ['rectangle']},  // Deprecated
+        'clip-path': {types: ['basic-shape']},
+        'mask': {properties: ['mask-position', 'mask-size']},
+        'mask-position': {types: ['length-percentage-calc'], multiple: true, repeatable: true},
+        'mask-size': {types: ['length-percentage-calc'], multiple: true, repeatable: true},
+
+        // CSS Multi-column Layout Module: http://www.w3.org/TR/css3-multicol/ + http://dev.w3.org/csswg/css-multicol/
+        'columns': {properties: ['column-width', 'column-count']},
+        'column-width': {types: ['length']},
+        'column-count': {types: ['integer']},
+        'column-gap': {types: ['length']},
+        'column-rule': {properties: ['column-rule-color', 'column-rule-width']},
+        'column-rule-color': {types: ['color']},
+        'column-rule-width': {types: ['length']},
+
+        // CSS Shapes Module Level 1: http://www.w3.org/TR/css-shapes-1/
         'shape-outside': {types: ['basic-shape']},
-        'shape-margin': {types: ['length', 'percentage', 'calc']},
+        'shape-margin': {types: ['length-percentage-calc']},
         'shape-image-threshold': {types: ['number']},
+
+        // CSS Text Decoration Module Level 3: http://www.w3.org/TR/css-text-decor-3/
+        'text-decoration': {properties: ['text-decoration-color']},
+        'text-decoration-color': {types: ['color']},
+        'text-emphasis': {properties: ['text-emphasis-color']},
+        'text-emphasis-color': {types: ['color']},
+        'text-shadow': {types: ['shadow-list']},
+
+
+        /*** Last Call Working Drafts (LC) ***/
+
+        // CSS Text Module Level 3: http://www.w3.org/TR/css3-text/
+        'letter-spacing': {types: ['length']},
+        'tab-size': {types: ['length']},
+        'text-indent': {types: ['length-percentage-calc']},
+        'word-spacing': {types: ['length-percentage-calc']},
+
+
+        /*** Working Drafts (WD) ***/
+
+        // CSS Flexible Box Layout Module Level 1: http://www.w3.org/TR/css3-flexbox/
+        'flex': {properties: ['flex-grow', 'flex-shrink', 'flex-basis']},
+        'flex-grow': {types: ['number']},
+        'flex-shrink': {types: ['number']},
+        'flex-basis': {types: ['length-percentage-calc']},
+        'order': {types: ['integer']},
+
+        // CSS Transforms Module Level 1: http://www.w3.org/TR/css3-transforms/
+        'transform': {types: ['transform']},
+        'transform-origin': {types: ['length-percentage-calc'], multiple: true},
+        'perspective': {types: ['length']},
+        'perspective-origin': {types: ['length-percentage-calc'], multiple: true},
     };
 
     /**
