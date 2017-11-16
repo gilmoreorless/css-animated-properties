b794a340572c8cc099c78482d47be1453430b078
Gilmore Davidson <gilmoreorless@gmail.com>
2017-11-16 11:08:41 +1100

    Updated data for latest CR specs (ui, flexbox, grid, scroll snap)
    Closes #2

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
  */
 (function (exports) {
     /**
-     * Data collated from multiple W3C specs: http://www.w3.org/Style/CSS/current-work
+     * Data collated from multiple W3C specs: https://www.w3.org/Style/CSS/current-work
      * Only specs that are Candidate Recommendations or better are counted, with the
      * exception of some Working Drafts that have a lot of traction in browser implementations.
-     * So far the WD specs included here are Text Level 3, Flexbox and Transforms.
+     * So far the WD specs included here are Multi-column Layout, Text Level 3, and Transforms.
      *
      * If a property is not in this list, it's not able to be consistently animated.
      * Technically `background-image` can be animated in some ways, but against the spec.
@@ -18,7 +18,7 @@
 
         /*** Recommendations (REC) ***/
 
-        // CSS 2.1: http://www.w3.org/TR/css3-transitions/#animatable-types
+        // CSS 2.1: https://www.w3.org/TR/css3-transitions/#animatable-types
         // --Box model
         'margin': {properties: ['margin-top', 'margin-right', 'margin-bottom', 'margin-left']},
         'margin-bottom': {types: ['length']},
@@ -50,14 +50,14 @@
         // --Tables
         'border-spacing': {types: ['length'], multiple: true},
 
-        // CSS Color Module Level 3: http://www.w3.org/TR/css3-color/
+        // CSS Color Module Level 3: https://www.w3.org/TR/css3-color/
         'color': {types: ['color']},
         'opacity': {types: ['number']},
 
 
         /*** Candidate Recommendations (CR) ***/
 
-        // CSS Backgrounds and Borders Module Level 3: http://www.w3.org/TR/css3-background/
+        // CSS Backgrounds and Borders Module Level 3: https://www.w3.org/TR/css3-background/
         'background': {properties: ['background-color', 'background-position', 'background-size']},
         'background-color': {types: ['color']},
         'background-position': {types: ['length-percentage-calc'], multiple: true, repeatable: true},
@@ -88,41 +88,73 @@
         // --Box shadow
         'box-shadow': {types: ['shadow-list']},
 
-        // CSS Basic User Interface Module Level 3 (CSS3 UI): http://www.w3.org/TR/css3-ui/
+        // CSS Basic User Interface Module Level 3 (CSS3 UI): https://www.w3.org/TR/css3-ui/
+        'caret-color': {types: ['color']},
         'outline': {properties: ['outline-color', 'outline-width']},
         'outline-color': {types: ['color']},
         'outline-width': {types: ['length']},
         'outline-offset': {types: ['length']},
 
-        // CSS Fonts Module Level 3: http://www.w3.org/TR/css3-fonts/
+        // CSS Flexible Box Layout Module Level 1: https://www.w3.org/TR/css3-flexbox/
+        'flex': {properties: ['flex-grow', 'flex-shrink', 'flex-basis']},
+        'flex-grow': {types: ['number']},
+        'flex-shrink': {types: ['number']},
+        'flex-basis': {types: ['length-percentage-calc']},
+        'order': {types: ['integer']},
+
+        // CSS Fonts Module Level 3: https://www.w3.org/TR/css3-fonts/
         'font': {properties: ['font-weight', 'font-stretch', 'font-size', 'line-height']},
         'font-weight': {types: ['font-weight']},
         'font-stretch': {types: ['font-stretch']},
         'font-size': {types: ['length']},
         'font-size-adjust': {types: ['number']},
 
-        // CSS Masking Module Level 1: http://www.w3.org/TR/css-masking/
+        // CSS Grid Layout Module Level 1: https://www.w3.org/TR/css-grid-1/
+        'grid-template-columns': {types: ['length-percentage-calc'], multiple: true},
+        'grid-template-rows': {types: ['length-percentage-calc'], multiple: true},
+        'grid-template': {properties: ['grid-template-rows', 'grid-template-columns']},
+        'grid': {properties: ['grid-template-rows', 'grid-template-columns']},
+        'grid-row-gap': {types: ['length-percentage-calc']},
+        'grid-column-gap': {types: ['length-percentage-calc']},
+        'grid-gap': {properties: ['grid-row-gap', 'grid-column-gap']},
+
+        // CSS Masking Module Level 1: https://www.w3.org/TR/css-masking/
         'clip': {types: ['rectangle']},  // Deprecated
         'clip-path': {types: ['basic-shape']},
         'mask': {properties: ['mask-position', 'mask-size']},
         'mask-position': {types: ['length-percentage-calc'], multiple: true, repeatable: true},
         'mask-size': {types: ['length-percentage-calc'], multiple: true, repeatable: true},
 
-        // CSS Multi-column Layout Module: http://www.w3.org/TR/css3-multicol/ + http://dev.w3.org/csswg/css-multicol/
-        'columns': {properties: ['column-width', 'column-count']},
-        'column-width': {types: ['length']},
-        'column-count': {types: ['integer']},
-        'column-gap': {types: ['length']},
-        'column-rule': {properties: ['column-rule-color', 'column-rule-width']},
-        'column-rule-color': {types: ['color']},
-        'column-rule-width': {types: ['length']},
-
-        // CSS Shapes Module Level 1: http://www.w3.org/TR/css-shapes-1/
+        // CSS Shapes Module Level 1: https://www.w3.org/TR/css-shapes-1/
         'shape-outside': {types: ['basic-shape']},
         'shape-margin': {types: ['length-percentage-calc']},
         'shape-image-threshold': {types: ['number']},
 
-        // CSS Text Decoration Module Level 3: http://www.w3.org/TR/css-text-decor-3/
+        // CSS Scroll Snap Module Level 1: https://www.w3.org/TR/css-scroll-snap-1/
+        'scroll-padding': {properties: ['scroll-padding-top', 'scroll-padding-right', 'scroll-padding-bottom', 'scroll-padding-left']},
+        'scroll-padding-top': {types: ['length-percentage-calc']},
+        'scroll-padding-right': {types: ['length-percentage-calc']},
+        'scroll-padding-bottom': {types: ['length-percentage-calc']},
+        'scroll-padding-left': {types: ['length-percentage-calc']},
+        'scroll-padding-block': {properties: ['scroll-padding-block-start', 'scroll-padding-block-end']},
+        'scroll-padding-block-start': {types: ['length-percentage-calc']},
+        'scroll-padding-block-end': {types: ['length-percentage-calc']},
+        'scroll-padding-inline': {properties: ['scroll-padding-inline-start', 'scroll-padding-inline-end']},
+        'scroll-padding-inline-start': {types: ['length-percentage-calc']},
+        'scroll-padding-inline-end': {types: ['length-percentage-calc']},
+        'scroll-snap-margin': {properties: ['scroll-snap-margin-top', 'scroll-snap-margin-right', 'scroll-snap-margin-bottom', 'scroll-snap-margin-left']},
+        'scroll-snap-margin-top': {types: ['length']},
+        'scroll-snap-margin-right': {types: ['length']},
+        'scroll-snap-margin-bottom': {types: ['length']},
+        'scroll-snap-margin-left': {types: ['length']},
+        'scroll-snap-margin-block': {properties: ['scroll-snap-margin-block-start', 'scroll-snap-margin-block-end']},
+        'scroll-snap-margin-block-start': {types: ['length']},
+        'scroll-snap-margin-block-end': {types: ['length']},
+        'scroll-snap-margin-inline': {properties: ['scroll-snap-margin-inline-start', 'scroll-snap-margin-inline-end']},
+        'scroll-snap-margin-inline-start': {types: ['length']},
+        'scroll-snap-margin-inline-end': {types: ['length']},
+
+        // CSS Text Decoration Module Level 3: https://www.w3.org/TR/css-text-decor-3/
         'text-decoration': {properties: ['text-decoration-color']},
         'text-decoration-color': {types: ['color']},
         'text-emphasis': {properties: ['text-emphasis-color']},
@@ -130,25 +162,24 @@
         'text-shadow': {types: ['shadow-list']},
 
 
-        /*** Last Call Working Drafts (LC) ***/
+        /*** Working Drafts (WD) ***/
 
-        // CSS Text Module Level 3: http://www.w3.org/TR/css3-text/
+        // CSS Multi-column Layout Module Level 1: https://www.w3.org/TR/css3-multicol/ (WD)
+        'columns': {properties: ['column-width', 'column-count']},
+        'column-width': {types: ['length']},
+        'column-count': {types: ['integer']},
+        'column-gap': {types: ['length-percentage-calc']},
+        'column-rule': {properties: ['column-rule-color', 'column-rule-width']},
+        'column-rule-color': {types: ['color']},
+        'column-rule-width': {types: ['length']},
+
+        // CSS Text Module Level 3: https://www.w3.org/TR/css3-text/
         'letter-spacing': {types: ['length']},
         'tab-size': {types: ['length']},
         'text-indent': {types: ['length-percentage-calc']},
         'word-spacing': {types: ['length-percentage-calc']},
 
-
-        /*** Working Drafts (WD) ***/
-
-        // CSS Flexible Box Layout Module Level 1: http://www.w3.org/TR/css3-flexbox/
-        'flex': {properties: ['flex-grow', 'flex-shrink', 'flex-basis']},
-        'flex-grow': {types: ['number']},
-        'flex-shrink': {types: ['number']},
-        'flex-basis': {types: ['length-percentage-calc']},
-        'order': {types: ['integer']},
-
-        // CSS Transforms Module Level 1: http://www.w3.org/TR/css3-transforms/
+        // CSS Transforms Module Level 1: https://www.w3.org/TR/css3-transforms/
         'transform': {types: ['transform']},
         'transform-origin': {types: ['length-percentage-calc'], multiple: true},
         'perspective': {types: ['length']},
@@ -157,63 +188,63 @@
 
     /**
      * List of animatable types used by properties, with descriptions of how to interpolate each type.
-     * Data taken from http://www.w3.org/TR/css3-transitions/#animatable-types and some other W3C specs.
+     * Data taken from https://www.w3.org/TR/css3-transitions/#animatable-types and some other W3C specs.
      *
      * @type {Object}
      */
     exports.types = {
         'color': {
             name: 'color',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-color'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-color'
         },
         'length': {
             name: 'length',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-length'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-length'
         },
         'percentage': {
             name: 'percentage',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-percentage'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-percentage'
         },
         'length-percentage-calc': {
             name: 'length, percentage, or calc',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-lpcalc'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-lpcalc'
         },
         'integer': {
             name: 'integer',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-integer'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-integer'
         },
         'font-weight': {
             name: 'font weight',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-font-weight'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-font-weight'
         },
         'number': {
             name: 'number',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-number'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-number'
         },
         'rectangle': {
             name: 'rectangle',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-rect'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-rect'
         },
         'visibility': {
             name: 'visibility',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-visibility'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-visibility'
         },
         'shadow-list': {
             name: 'shadow list',
-            href: 'http://www.w3.org/TR/css3-transitions/#animtype-shadow-list'
+            href: 'https://www.w3.org/TR/css3-transitions/#animtype-shadow-list'
         },
         // Other specs
         'transform': {
             name: 'transform',
-            href: 'http://www.w3.org/TR/css3-transforms/#interpolation-of-transforms'
+            href: 'https://www.w3.org/TR/css3-transforms/#interpolation-of-transforms'
         },
         'font-stretch': {
             name: 'font stretch',
-            href: 'http://www.w3.org/TR/css3-fonts/#font-stretch-animation'
+            href: 'https://www.w3.org/TR/css3-fonts/#font-stretch-animation'
         },
         'basic-shape': {
             name: 'basic shape',
-            href: 'http://www.w3.org/TR/css-shapes-1/#basic-shape-interpolation'
+            href: 'https://www.w3.org/TR/css-shapes-1/#basic-shape-interpolation'
         },
     };
 
